27a470c87855eb1a7d2e32cf48a087e8e460d490
Rita-anne <annegakiza@gmail.com>
2024-02-21 08:32:36 +0200

    FFFF

---
+++ fashion1/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { TextInput } from 'react-native-paper';
+import { StyleSheet, Text, View , ScrollView} from 'react-native';
+
+export default function App() {
+  return (
+    <ScrollView>
+      
+    </ScrollView>
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ fashion1/Src.jsx
@@ -0,0 +1,38 @@
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+export default function ScrollView({word,style}) {
+    return(
+        <View>
+            <Text>
+
+
+
+                {word}
+            </Text>
+        </View>
+    );
+}
+
+const styles=StyleSheet.create({
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
