0dc0d991948bc6f00e6903accb89691f48ec9156
Rita-anne <annegakiza@gmail.com>
2024-02-22 07:02:48 +0200

    fff

--- fashion1/App.js
+++ fashion1/App.js
@@ -1,24 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
 import { TextInput } from 'react-native-paper';
 import { StyleSheet, Text, View , ScrollView} from 'react-native';
-
+import Src from '../fashion1/Src'
 export default function App() {
+  const fash =[
+    {type:"pants",
+    price:"$",
+    clothes:"https://tse1.mm.bing.net/th?id=OIP.odOmvkbLK5gBUQujCLdBIwHaKq&pid=Api&P=0&h=220"
+  },
+  {type:"shirts",
+    price:"$",
+    clothes:"https://tse1.mm.bing.net/th?id=OIP.odOmvkbLK5gBUQujCLdBIwHaKq&pid=Api&P=0&h=220"
+  },
+  {type:"skirts",
+    price:"$",
+    clothes:"https://tse1.mm.bing.net/th?id=OIP.odOmvkbLK5gBUQujCLdBIwHaKq&pid=Api&P=0&h=220"
+  },
+
+  ];
+    
+  
   return (
-    <ScrollView>
+    // <ScrollView>
       
-    </ScrollView>
+    // </ScrollView>
     // <View style={styles.container}>
     //   <Text>Open up App.js to start working on your app!</Text>
     //   <StatusBar style="auto" />
     // </View>
+    <View>
+      <ScrollView horizontal>
+{fash.map((item,index)=>{
+return(
+  <View>
+<Src type={item.type} price={item.price} clothes={item.clothes}  />
+  </View>
+)}
+)}
+</ScrollView>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  //   backgroundColor: '#fff',
-  //   alignItems: 'center',
-  //   justifyContent: 'center',
-  // },
-});
+// const styles = StyleSheet.create({
+//   // container: {
+//   //   flex: 1,
+//   //   backgroundColor: '#fff',
+//   //   alignItems: 'center',
+//   //   justifyContent: 'center',
+//   // },
+// });
--- fashion1/Src.jsx
+++ fashion1/Src.jsx
@@ -1,13 +1,20 @@
-import { StyleSheet, Text, View, ScrollView } from "react-native";
-export default function ScrollView({word,style}) {
+import { StyleSheet, Text, View, ScrollView,ImageBackground} from "react-native";
+export default function Fash({type,price,clothes}) {
     return(
         <View>
-            <Text>
-
-
-
-                {word}
-            </Text>
+<ImageBackground style={{width:200, height:200, margin:100}} source={{uri:clothes}} >
+<Text style={{color:'white'}} >
+    #best seller
+</Text>
+<View style={{height:80,backgroundColor:'rgba(000,000,000,0.5)'}} >
+<Text style={{color:'white'}}>
+    {type}
+</Text>
+<Text style={{color:'white'}}>
+    {price}
+</Text>
+</View>
+</ImageBackground>
         </View>
     );
 }
